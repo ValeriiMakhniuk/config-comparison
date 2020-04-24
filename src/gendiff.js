6f5d2376567f1ce7a34143b823053eb1c59f3a61
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-04-24 14:41:44 +0300

    change namings and apply getParser function to files data

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,16 +1,19 @@
 import createDiff from './createDiff';
 import stringify from './stringify';
+import getParser from './parsers';
 
 const fs = require('fs');
 const path = require('path');
 
 export default function genDiff(pathToFile1, pathToFile2) {
-  const actualPathToFirstFile = path.resolve(process.cwd(), pathToFile1);
-  const actualPathToSecondFile = path.resolve(process.cwd(), pathToFile2);
-  const firstFileData = fs.readFileSync(actualPathToFirstFile, 'utf-8');
-  const secondFileData = fs.readFileSync(actualPathToSecondFile, 'utf-8');
-  const firstJSON = JSON.parse(firstFileData);
-  const secondJSON = JSON.parse(secondFileData);
-  const rawDiff = createDiff(firstJSON, secondJSON);
+  const fullPathToFile1 = path.resolve(process.cwd(), pathToFile1);
+  const fullPathToFile2 = path.resolve(process.cwd(), pathToFile2);
+  const extNameFile1 = path.extname(fullPathToFile1);
+  const extNameFile2 = path.extname(fullPathToFile2);
+  const File1Data = fs.readFileSync(fullPathToFile1, 'utf-8');
+  const File2Data = fs.readFileSync(fullPathToFile2, 'utf-8');
+  const File1ParsedData = getParser(extNameFile1)(File1Data);
+  const File2ParsedData = getParser(extNameFile2)(File2Data);
+  const rawDiff = createDiff(File1ParsedData, File2ParsedData);
   return stringify(rawDiff);
 }
