0efb36fdc469329748709b1b28e06a82e7291404
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-04-27 14:08:32 +0300

    add JSON formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,13 @@
 import base from './base';
 import plain from './plain';
+import json from './json';
 
 const getFormat = (format) => {
   switch (format) {
     case 'plain':
-      return base;
+      return plain;
+    case 'json':
+      return json;
     case 'base':
     default:
       return base;
---
+++ src/formatters/json.js
@@ -0,0 +1,5 @@
+const formatDiff = (rawDiff) => {
+  return JSON.stringify(rawDiff, null, ' ');
+};
+
+export default formatDiff;
