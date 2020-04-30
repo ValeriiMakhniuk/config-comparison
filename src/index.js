af2051536585b3bcac78bd457fe5ec8d75c31848
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-04-30 14:50:29 +0300

    [fix] toLowercase var names

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ export default function genDiff(pathToFile1, pathToFile2, format) {
   const fullPathToFile2 = resolve(process.cwd(), pathToFile2);
   const extNameFile1 = extname(fullPathToFile1).slice(1);
   const extNameFile2 = extname(fullPathToFile2).slice(1);
-  const File1Data = readFileSync(fullPathToFile1, 'utf-8');
-  const File2Data = readFileSync(fullPathToFile2, 'utf-8');
-  const File1ParsedData = getParser(extNameFile1)(File1Data);
-  const File2ParsedData = getParser(extNameFile2)(File2Data);
-  const diffTree = createDiffTree(File1ParsedData, File2ParsedData, START_DEPTH_LEVEL);
+  const file1Data = readFileSync(fullPathToFile1, 'utf-8');
+  const file2Data = readFileSync(fullPathToFile2, 'utf-8');
+  const file1ParsedData = getParser(extNameFile1)(file1Data);
+  const file2ParsedData = getParser(extNameFile2)(file2Data);
+  const diffTree = createDiffTree(file1ParsedData, file2ParsedData, START_DEPTH_LEVEL);
   return getFormat(format)(diffTree);
 }
