8ef31fb7bfabddb8b2f930170f54e0a446fd2c04
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-05-12 12:56:37 +0300

    make formater function recursive

--- src/formatters/pretty.js
+++ src/formatters/pretty.js
@@ -3,7 +3,6 @@ import isPlainObject from 'lodash/isPlainObject';
 const INDENT_TYPE = ' ';
 const INDENT_SIZE = 4;
 const BASIC_INDENT_SIZE = 2;
-const START_DEPTH_LEVEL = 0;
 const marks = {
   added: '+',
   removed: '-',
@@ -28,36 +27,37 @@ const stringify = (value, depth) => {
     .join('\n');
 };
 
-const formatNode = (node, depth) => {
-  const {
-    key, value, state,
-  } = node;
+const formatDiff = (rawDiff, startDepth) => {
+  const formatNode = (node, depth) => {
+    const {
+      key, value, state,
+    } = node;
 
-  const currentIndent = INDENT_TYPE.repeat(depth * INDENT_SIZE + BASIC_INDENT_SIZE);
+    const currentIndent = INDENT_TYPE.repeat(depth * INDENT_SIZE + BASIC_INDENT_SIZE);
 
-  switch (state) {
-    case 'added':
-      return `${currentIndent}${marks.added} ${key}: ${stringify(value, depth + 1)}`;
-    case 'removed':
-      return `${currentIndent}${marks.removed} ${key}: ${stringify(value, depth + 1)}`;
-    case 'changed':
-      return ([
-        `${currentIndent}${marks.removed} ${key}: ${stringify(value[0], depth + 1)}`,
-        `${currentIndent}${marks.added} ${key}: ${stringify(value[1], depth + 1)}`,
-      ].join('\n'));
-    case 'nested':
-      return ([
-        `${currentIndent}${INDENT_TYPE} ${key}: {`,
-        `${node.value.map((childNode) => formatNode(childNode, depth + 1)).join('\n')}`,
-        `${currentIndent} ${INDENT_TYPE}}`,
-      ].join('\n'));
-    case 'unchanged':
-      return `${currentIndent}${marks.unchanged} ${key}: ${stringify(value, depth + 1)}`;
-    default:
-      throw new Error(`Unknown state: ${state}`);
-  }
+    switch (state) {
+      case 'added':
+        return `${currentIndent}${marks.added} ${key}: ${stringify(value, depth + 1)}`;
+      case 'removed':
+        return `${currentIndent}${marks.removed} ${key}: ${stringify(value, depth + 1)}`;
+      case 'changed':
+        return ([
+          `${currentIndent}${marks.removed} ${key}: ${stringify(value.before, depth + 1)}`,
+          `${currentIndent}${marks.added} ${key}: ${stringify(value.after, depth + 1)}`,
+        ].join('\n'));
+      case 'nested': return `${currentIndent}${INDENT_TYPE} ${key}: ${formatDiff(node.children, depth + 1)}`;
+      case 'unchanged':
+        return `${currentIndent}${marks.unchanged} ${key}: ${stringify(value, depth + 1)}`;
+      default:
+        throw new Error(`Unknown state: ${state}`);
+    }
+  };
+
+  return [
+    '{',
+    `${rawDiff.map((node) => formatNode(node, startDepth)).join('\n')}`,
+    `${INDENT_TYPE.repeat(startDepth * INDENT_SIZE)}}`,
+  ].join('\n');
 };
 
-const formatDiff = (rawDiff) => `{\n${rawDiff.map((node) => formatNode(node, START_DEPTH_LEVEL)).join('\n')}\n}`;
-
-export default formatDiff;
+export default (rawDiff) => formatDiff(rawDiff, 0);
